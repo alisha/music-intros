5c521718d11c361a255083e9de5108e2e74cf890
Alisha Ukani <alishaaukani@gmail.com>
2017-07-23 17:17:28 -0400

    Use last.fm API instead of RSpotify

--- music-intro.rb
+++ music-intro.rb
@@ -5,9 +5,11 @@ require 'rspotify'
 # uncomment the line below for running on local machines, where secret.rb contains API keys
 require_relative 'secret'
 
-
 # get config vars (API keys) from Heroku
 # Y_API_KEY = ENV['Y_API_KEY']
+# S_API_KEY   = ENV['S_API_KEY']
+# S_SECRET    = ENV['S_SECRET']
+# LF_API_KEY  = ENV['LF_API_KEY']
 
 error 400..510 do
   redirect to('/error')
@@ -51,79 +53,65 @@ get '/artist/:artist' do
   artistName = params[:artist]
   
   # get name, photo, and genres
-  artists = RSpotify::Artist.search(artistName)
-  
-  if artists.nil? || artists.length == 0
-    redirect to ("/error")
-  end
-
-  artist = artists.first
-  
-  @artistName = artist.name
-  
-  if artist.images.length > 0 && artist.images[0]["url"] != ""
-    @photoURL = artist.images[0]["url"]
-  end
-  
-  if artist.genres.length > 0
-    @genres = artist.genres.join(", ")
-  end
-  
-  # get related artists
-  relatedArtistsList = artist.related_artists
-
-  @relatedArtists = Array.new
-  
-  if relatedArtistsList.nil? || relatedArtistsList.length > 0
-    while @relatedArtists.length < 5 && @relatedArtists.length < relatedArtistsList.length - 1
-      @relatedArtists.push(relatedArtistsList[@relatedArtists.length].name)
-    end
-
-    @relatedArtists = @relatedArtists.join(", ")
-  else
-    @relatedArtists = ""
-  end
-
-  # get artist's most popular songs
-  topTracksList = artist.top_tracks(:US)
-
-  @topTracks = Array.new
-  @youTubeURLs = Array.new
-
-  if topTracksList.length > 0
-
-    while @topTracks.length < 5 and @topTracks.length < topTracksList.length - 1
-      name = topTracksList[@topTracks.length].name
-
-      # get song title
-      @topTracks.push(name)
-
-      # find YouTube video id
-      query = "#{name} #{@artistName}".gsub(/[^0-9a-zA-Z ]/i, '').gsub(" ", "+")
-      songURLrequest = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{query}&type=video&key=#{Y_API_KEY}")
-
-      url = JSON.parse(songURLrequest.body)["items"][0]["id"]["videoId"]
-
-      @youTubeURLs.push(url)
-
-    end
-  end
-
-  # make sure @genres is not just a new line or some spaces
-  if @genres.nil? || @genres.sub(/[ \t]{2,}\z/, '') == ""
-    @genres = ""
-  end
-
-  # get artist biography from last.fm
-  biographyRequest = HTTParty.get("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo", :query => {
+  artistRequest = HTTParty.get("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo", :query => {
     :api_key => LF_API_KEY,
     :artist => @artistName,
     :format => "json"
   })
 
-  biographyResponse = JSON.parse(biographyRequest.body)
+  artistResponse = JSON.parse(artistRequest.body)
+  puts artistResponse
+  artist = artistResponse["artist"]
 
-  @biography = biographyResponse["artist"]["bio"]["content"]
+  @artistName = artist["name"]
+
+  # get picture, try for largest photo possible
+  @photoURL = artist["image size=\"large\""]
+  if @photoURL.nil?
+    @photoURL = artist["image size=\"medium\""]
+  end
+  if @photoURL.nil?
+    @photoURL = artist["image size=\"small\""]
+  end
+
+  @biography = artist["bio"]["content"]
+
+  # get genres
+  @genres = []
+  for genre in artist.tags
+    @genres.push(genre["name"])
+  end
+
+  # get related artists
+  @relatedArtists = []
+  for band in artist.similar
+    @relatedArtists.push(band["name"])
+  end
+
+  # get top tracks
+  topTracksRequest = HTTParty.get("https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks", :query => {
+    :api_key => LF_API_KEY,
+    :artist => @artistName,
+    :limit => 5,
+    :format => "json"
+  })
+
+  topTracksResponse = JSON.parse(topTracksRequest.body)
+  
+  @topTracks = []
+  @youTubeURLs = []
+
+  (1..5).each do |i|
+    # song titles from last.fm
+    title = topTracksResponse["track rank=\"#{i}\""]["name"]
+    @topTracks.push(title)
+
+    # find song on YouTube
+    query = "#{title} #{@artistName}".gsub(/[^0-9a-zA-Z ]/i, '').gsub(" ", "+")
+    songURLrequest = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{query}&type=video&key=#{Y_API_KEY}")
+    url = JSON.parse(songURLrequest.body)["items"][0]["id"]["videoId"]
+    @youTubeURLs.push(url)
+  end
 
   erb :artist
 
